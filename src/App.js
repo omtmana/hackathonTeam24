06060b2c098426ecbb397f1ff6dc907d1a9597cf
Eriche Gonzales <erichelouise@gmail.com>
2022-10-01 17:51:19 -0400

    bootstrapped navbar

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import Contact from './components/Contact';
 import Home from './components/Home';
 import Questions from './components/Questions';
 import AvailableHousing from './components/AvailableHousing';
+import { Nav, Navbar, Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">HouseHaven</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/questions">Questions</Nav.Link>
+              <Nav.Link href="/availableHousing">Housing</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Router>
-        <nav className='nav'>
-          <Link to='/'> Home </Link>
-          <Link to='/contact'> Contact </Link>
-          <Link to='/questions'> Questions </Link>
-          <Link to='/availableHousing'> Housing </Link>
-        </nav>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/questions' element={<Questions />}/>
-          <Route path='/availableHousing' element={<AvailableHousing />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/availableHousing" element={<AvailableHousing />} />
         </Routes>
       </Router>
     </div>

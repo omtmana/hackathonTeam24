b2175c84a73580f991380b93e6b1c510114680ed
Olivia <oliviatmanalastas@gmail.com>
2022-10-02 13:19:44 -0400

    designed home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,17 @@ import '../style/home.css'
 const Home = () => {
    return (
       <div className="home">
-         <img src='woman.jpeg' className='home-image' />  
+         {/* <img src='logo.png' className='home-image' />   */}
          <div className='home-header'>
-            <h1 className='home-font'> Team 24: <br></br> Empowering Women </h1>
-            <p> In this part we are going to put a brief information on what our goal is for this website </p>       
+            <h1> A Fresh start for <br></br> Justice-Involved Women</h1>
+            <p> We help you find a safe and secure home for your family </p>
+            <button className='home-button'> How it works </button>
+            {/* <h1 className='home-font'> Team 24: <br></br> Empowering Women </h1>
+            <p> In this part we are going to put a brief information on what our goal is for this website </p>        */}
          </div>
          <div className='home-body'>
-            <img src='flowchart.png' style={{heigh:'4em'}}></img>
-         </div>
-         <div className='home-footer'>
-            <h5> This is the home footer </h5>
+            {/* <img src='flowchart.png' style={{heigh:'4em'}}></img> */}
+            <img className='home-image' src='home.png' />
          </div>
       </div>
    )

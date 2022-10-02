8572bc4e81cbc4b1faa9ea1be910b2bbd548bb14
Olivia <oliviatmanalastas@gmail.com>
2022-10-02 09:51:07 -0400

    morning commit

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -6,7 +6,7 @@ const Questions = () => {
       <div>
          <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-               <Form.Label>Email address</Form.Label>
+               <Form.Label>First Name</Form.Label>
                <Form.Control type="email" placeholder="Enter email" />
                <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
